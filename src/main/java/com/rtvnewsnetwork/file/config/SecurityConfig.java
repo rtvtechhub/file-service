6a65d1ca1fe36178b4f10dc0b0932dc695f6dd83
abdurrakib321 <abdur@zendot.in>
2025-08-14 01:47:59 +0530

    code clean up

--- src/main/java/com/rtvnewsnetwork/file/config/SecurityConfig.java
+++ src/main/java/com/rtvnewsnetwork/file/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("localhost:4400" ,"https://mobile.dev.rtvlive.com"   ));
+        config.setAllowedOrigins(List.of("localhost:4400" ,"https://mobile.dev.rtvlive.com"  ,"https://mobile.file.dev.rtvlive.com" ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
